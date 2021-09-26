e48e50200aed506d64855f162be23b646ad4af97
Time_V5 <591089834@qq.com>
2021-09-26 22:17:55 +0800

    2021-09-26 21:57:56 commit

--- score.py
+++ score.py
@@ -8,11 +8,6 @@ with open(sys.argv[1]) as f:
             map(float, re.findall("Score: (.*?),", readlines))
         )
     ))
-    print("Max step: %d"%max(
-        [0]+list(
-            map(int, re.findall("Step: (.*?),", readlines))
-        )
-    ))
     print("Finish: %d"%sum(
         [0]+list(
             map(bool, re.findall("Evaluation: (success?).", readlines))
