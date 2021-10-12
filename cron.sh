846a9a7f72d52407eff113d1f8fae93f96bd7c93
Time_V5 <591089834@qq.com>
2021-10-12 21:27:32 +0800

    2021-10-12 21:17:09 commit

--- cron.sh
+++ cron.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-eval $(date +'%H %M'|awk '{printf "H=%s\nM=%s\n",$1,$2}')
-[ "$(egrep '.*train.sh.*' /etc/crontab)" != "" ] && \
-sed -i "s/.*train.sh.*/$[($M+30)%60] $[(($M+30)/60+$H)%24] \* \* \* root \/root\/Agent-contra\/train\.sh/g" /etc/crontab || \
-echo "$[(`expr $M + 0`+30)%60] $[((`expr $M + 0`+30)/60+`expr $H + 0`)%24] * * * root /root/Agent-contra/train.sh" >> /etc/crontab
+path="/root/Agent-contra"
+eval $(date +'%d %H %M'|awk '{printf "D=$[10#%s+0]\nH=$[10#%s+0]\nM=$[10#%s+0]\n",$1,$2,$3}')
+echo "$[($M+30)%60] $[(($M+30)/60+$H)%24] $[$D+(($M+30)/60+$H)/24] * * root $path/train.sh $1" >> /etc/crontab
 rm -rf log records tensorboard utils/__pycache__ nohup.out score
 read -p "Restart now, no/yes?(default no): " restart
 case ${restart,,} in
