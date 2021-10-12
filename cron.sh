7de923d6ca2106c41f24dc50a0424af6211ac846
Time_V5 <591089834@qq.com>
2021-10-12 21:28:55 +0800

    2021-10-12 21:17:09 commit

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@
 path="/root/Agent-contra"
 eval $(date +'%d %H %M'|awk '{printf "D=$[10#%s+0]\nH=$[10#%s+0]\nM=$[10#%s+0]\n",$1,$2,$3}')
 echo "$[($M+30)%60] $[(($M+30)/60+$H)%24] $[$D+(($M+30)/60+$H)/24] * * root $path/train.sh $1" >> /etc/crontab
-rm -rf log records tensorboard utils/__pycache__ nohup.out score
+rm -rf $path/log $path/records $path/tensorboard
 read -p "Restart now, no/yes?(default no): " restart
 case ${restart,,} in
 "yes"|"y") reboot;;
